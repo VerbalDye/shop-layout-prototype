9acc43a4574c50661a95794c71ccdaf7d7499862
Dustin Huntsberger <dustin.huntsberger@movilitas.com>
2022-11-30 13:09:07 -0500

    updated syle

--- index.js
+++ index.js
@@ -13,28 +13,31 @@ shopObjectEl = document.createElement("div");
 shopObjectEl.className = "shop-element";
 shopLayoutEl.appendChild(shopObjectEl);
 
-var beginClick = function(event) {
-    moving = true;
-    movingLoop(event.target);
-}
+// var beginClick = function(event) {
+//     console.log("yes")
+//     moving = true;
+//     movingLoop(event.target);
+// }
 
-var endClick = function(event) {
-    moving = false;
-}
+// var endClick = function(event) {
+//     console.log("no")
+//     moving = false;
+// }
 
-var movingLoop = function(target) {
-    while(moving == true) {
-        if(target.className == "shop-element") {
-            target.style.background = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
-        }
-    }
-}
+// var movingLoop = async function(target) {
+//     while(moving == true) {
+//         console.log("go");
+//         if(target.className == "shop-element") {
+//             target.style.background = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+//         }
+//     }
+// }
 
 var handleSave = function(event) {
     shopLayoutEl.style.width = widthInputEl.value + "px"
     shopLayoutEl.style.height = heightInputEl.value + "px"
 }
 
-shopLayoutEl.addEventListener('mousedown', beginClick);
-shopLayoutEl.addEventListener('mouseup', endClick)
+// shopLayoutEl.addEventListener('mousedown', beginClick);
+// shopLayoutEl.addEventListener('mouseup', endClick)
 saveBtn.addEventListener('click', handleSave);
